5f2058a3790653a48531356cec5aea8376ae9b0e
Vadzim Bykau <bvntaev@gmail.com>
2023-03-10 15:48:55 +0300

    feat: create ios native module

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTCalendarModule.h
+//  FlipperProject
+//
+//  Created by Vadzim on 10.03.23.
+//
+
+#ifndef RCTCalendarModule_h
+#define RCTCalendarModule_h
+
+
+#endif /* RCTCalendarModule_h */
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTCalendarModule.m
+//  FlipperProject
+//
+//  Created by Vadzim on 10.03.23.
+//
+
+#import <Foundation/Foundation.h>
