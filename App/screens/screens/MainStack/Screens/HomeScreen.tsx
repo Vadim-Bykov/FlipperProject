f8f479b2cd51c0d06892b5ec6a1931bc241211ef
Vadzim Bykau <bvntaev@gmail.com>
2023-03-10 15:49:34 +0300

    feat: create ios native module

--- App/screens/screens/MainStack/Screens/HomeScreen.tsx
+++ App/screens/screens/MainStack/Screens/HomeScreen.tsx
@@ -1,10 +1,28 @@
 import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {MainStackProps} from '../MainStackNavigationType';
+import NativeCalendarModule from '../../../../modules/NativeCalendarModule';
 
 export const HomeScreen: React.FC<MainStackProps<'HomeScreen'>> = ({
   navigation: {navigate},
 }) => {
+  const createCalendarEvent = () => {
+    NativeCalendarModule.createCalendarEvent('Cool party', 'Mogilev');
+    NativeCalendarModule.createCalendarEventCallback(
+      'Cool party',
+      'Mogilev',
+      (error, eventId) => {
+        if (error) {
+          console.error(`Error found! ${error}`);
+        }
+        console.log(`Callback with an event id ${eventId} returned`);
+      },
+    );
+    console.log({
+      DEFAULT_EVENT_NAME: NativeCalendarModule.getConstants(),
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
@@ -17,6 +35,8 @@ export const HomeScreen: React.FC<MainStackProps<'HomeScreen'>> = ({
         title="Go to PostsScreen"
         onPress={() => navigate('PostsScreen')}
       />
+
+      <Button title="Calendar event" onPress={createCalendarEvent} />
     </View>
   );
 };
--- ios/RCTCalendarModule.h
+++ ios/RCTCalendarModule.h
@@ -1,12 +1,4 @@
-//
 //  RCTCalendarModule.h
-//  FlipperProject
-//
-//  Created by Vadzim on 10.03.23.
-//
-
-#ifndef RCTCalendarModule_h
-#define RCTCalendarModule_h
-
-
-#endif /* RCTCalendarModule_h */
+#import <React/RCTBridgeModule.h>
+@interface RCTCalendarModule : NSObject <RCTBridgeModule>
+@end
